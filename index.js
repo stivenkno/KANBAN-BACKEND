7cab78d2b10d83c5fe41eeddb48bd00c6e9db4df
stivenkno <stivencano931@gmail.com>
2025-04-22 16:06:50 -0500

    Rutas tareas implementadas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { PORT } from "./varsconfig.js";
 import authrouter from "./routes/authroutes.js";
 import columnsrouter from "./routes/columnsroutes.js";
 import projectsrouter from "./routes/projectsroutes.js";
+import tasksrouter from "./routes/tasksroutes.js";
 import authmiddleware from "./middlewares/authmiddleware.js";
 
 import pool from "./config.js";
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use("/auth", authrouter);
 app.use("/columns", authmiddleware, columnsrouter);
 app.use("/projects", authmiddleware, projectsrouter);
+app.use("/tasks", authmiddleware, tasksrouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
